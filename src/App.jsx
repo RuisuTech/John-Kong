3d620f63e3a8577ab9189d40be31f0c9fe138f03
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2025-06-17 23:53:02 -0500

    Views

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home.jsx";
 import Login from "./views/Login.jsx";
+import Password from "./views/Password.jsx";
+import Register from "./views/Register.jsx";
+import Reserva from "./views/Reserva.jsx";
+import Historial from "./views/Historial.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 import "./App.css";
 
 function App() {
@@ -10,6 +15,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Password" element={<Password />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Reserva" element={<Reserva />} />
+          <Route path="/Historial" element={<Historial />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </>
